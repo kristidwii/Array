7e2eb68784dcd78e850b0ed18d3d93487fca8a95
kristidwii <111744939+kristidwii@users.noreply.github.com>
2022-10-03 20:09:57 +0700

    Add files via upload

---
+++ array-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main (){
+int angka [] = {3,5,6,8,12};
+cout <<"angka"<< angka[4]<<endl; 
+char huruf[] = {'x','y','z'};
+printf ("huruf %c",huruf [1]);
+}
